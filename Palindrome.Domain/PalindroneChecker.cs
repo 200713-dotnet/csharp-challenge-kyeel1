d88f1c8efa4786defeda97451ee02ac304433d0c
henry <kyeel123@gmail.com>
2020-07-20 16:56:45 -0400

    adding to git

---
+++ Palindrome.Domain/PalindroneChecker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Palindrome.Domain
+{
+    public class PalindroneChecker
+    {
+        public int Middle(string x){
+            int result;
+            if (x.Length%2 == 0){
+            result = (x.Length)/2;
+            return result;
+            }
+            else{
+            result = (x.Length/2)-1;
+            return result;
+            }
+
+        }
+        public bool Check(string x, int index,int lastIndex)//returns true if ture or false if false
+        {
+            if(index == Middle(x))
+            {
+                if(x.Length%2 == 0)
+                {
+                    if(x[index] == x[lastIndex])
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            if(x[index] == x[lastIndex]){
+                Check(x,index+=1,lastIndex+=1);
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- Palindrome.Testing/UnitTest1.cs
+++ Palindrome.Testing/UnitTest1.cs
@@ -6,9 +6,64 @@ namespace Palindrome.Testing
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestSimple()
         {
+            var palindrome = "tacocat";
+            var index = 0;
+            var lastIndex = palindrome.Length;
+            var PC = new PalindromeChecker();
 
+            var check = PC.Check(palindrome,index,lastIndex);
+
+            Assert.isTrue(check);
+        }
+        [Fact]
+        public void TestFailure()
+        {
+            var palindrome = "tacoeecat";
+            var index = 0;
+            var lastIndex = palindrome.Length;
+            var PC = new PalindromeChecker();
+
+            var check = PC.Check(palindrome,index,lastIndex);
+
+            Assert.isTrue(check);
+        }
+        [Fact]
+        public void TestEven()
+        {
+            var palindrome = "anna";
+            var index = 0;
+            var lastIndex = palindrome.Length;
+            var PC = new PalindromeChecker();
+
+            var check = PC.Check(palindrome,index,lastIndex);
+
+            Assert.isTrue(check);
+        }
+        [Fact]
+        public void TestSymbol()
+        {
+            var palindrome = "^anna^";
+            var index = 0;
+            var lastIndex = palindrome.Length;
+            var PC = new PalindromeChecker();
+
+            var check = PC.Check(palindrome,index,lastIndex);
+
+            Assert.isTrue(check);
+        }
+        [Fact]
+        public void TestNumber()
+        {
+            var palindrome = "1331";
+            var index = 0;
+            var lastIndex = palindrome.Length;
+            var PC = new PalindromeChecker();
+
+            var check = PC.Check(palindrome,index,lastIndex);
+
+            Assert.isTrue(check);
         }
     }
 }
